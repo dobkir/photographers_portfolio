6d6358409ad1b5ccbac22dc71f92a225df913fd0
dobkir <dobkir@gmail.com>
2022-02-07 15:42:24 +0400

    feat: add video player feature

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import smoothScrollFunction from './smooth-scroll.js'
 import mobileMenuFunction from './mobile-menu.js'
 import preloadPortfolioImages from './preload-portfolio-images.js'
 import { switchPortfolioGallery } from './portfolio-galleries-switcher.js'
+import playVideo from './video-player.js';
 
 
 
@@ -9,3 +10,4 @@ smoothScrollFunction()
 mobileMenuFunction()
 preloadPortfolioImages()
 switchPortfolioGallery()
+playVideo()
